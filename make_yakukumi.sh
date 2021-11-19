e125a1d2adac126821a53d095f803b2ec9006d22
Daiji256 <butty.main@gmail.com>
2021-11-19 19:57:28 +0900

    change to make directly from ttc

--- make_yakukumi.sh
+++ make_yakukumi.sh
@@ -2,9 +2,10 @@
 
 mkdir -p yaku-kumi
 
-for file in `find noto-cjk/otf/ -name '*.otf'`; do
+for file in `find noto-cjk/ttc/ -name '*.ttc'`; do
     fname=`basename $file`
     outname=${fname/Noto/YakuKumi}
     outname=${outname/CJKjp/}
-    pyftsubset ${file} --text-file=chars.txt --layout-features='*' --output-file=yaku-kumi/${outname}
+    outname=${outname/ttc/otf}
+    pyftsubset ${file} --font-number=0 --text-file=chars.txt --layout-features='*' --output-file=yaku-kumi/${outname}
 done
