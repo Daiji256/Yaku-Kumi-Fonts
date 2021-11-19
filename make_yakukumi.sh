82a7b52744ce5943c8bec2ebdcc2490a18990985
Daiji256 <butty.main@gmail.com>
2021-11-19 20:49:37 +0900

    delete chars

--- make_yakukumi.sh
+++ make_yakukumi.sh
@@ -7,7 +7,7 @@ for file in `find noto-cjk/ -name '*.ttc'`; do
     outname=${outname/Noto/YakuKumi}
     outname=${outname/CJK/}
     outname=${outname/ttc/otf}
-    pyftsubset ${file} --font-number=0 --text-file=chars.txt --layout-features='*' --output-file=yaku-kumi/${outname}
+    pyftsubset ${file} --font-number=0 --text="、，。．〈〉《》「」『』【】〔〕（）［］｛｝" --layout-features='*' --output-file=yaku-kumi/${outname}
     familyname=${outname}
     familyname=${familyname/YakuKumiSans-/Yaku Kumi Sans }
     familyname=${familyname/.otf/}
