9ac6ac259ca55c66b9789dbfab0612f279665d1a
Daiji256 <butty.main@gmail.com>
2021-11-19 20:22:22 +0900

    rename fontname

--- make_yakukumi.sh
+++ make_yakukumi.sh
@@ -8,4 +8,8 @@ for file in `find noto-cjk/ -name '*.ttc'`; do
     outname=${outname/CJK/}
     outname=${outname/ttc/otf}
     pyftsubset ${file} --font-number=0 --text-file=chars.txt --layout-features='*' --output-file=yaku-kumi/${outname}
+    familyname=${outname}
+    familyname=${familyname/YakuKumiSans-/Yaku Kumi Sans }
+    familyname=${familyname/.otf/}
+    python3 fontname.py "${familyname}" yaku-kumi/${outname}
 done
