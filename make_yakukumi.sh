8519748d002ea4d42a836fbb169129d97a14f70b
Daiji256 <butty.main@gmail.com>
2021-11-19 20:11:14 +0900

    fix

--- make_yakukumi.sh
+++ make_yakukumi.sh
@@ -5,7 +5,7 @@ mkdir -p yaku-kumi
 for file in `find noto-cjk/ -name '*.ttc'`; do
     outname=`basename $file`
     outname=${outname/Noto/YakuKumi}
-    outname=${outname/CJKjp/}
+    outname=${outname/CJK/}
     outname=${outname/ttc/otf}
     pyftsubset ${file} --font-number=0 --text-file=chars.txt --layout-features='*' --output-file=yaku-kumi/${outname}
 done
