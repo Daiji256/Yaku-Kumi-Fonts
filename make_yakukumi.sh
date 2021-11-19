ef6913dca5daaae202b815fdea89e16202b29b5b
Daiji256 <butty.main@gmail.com>
2021-11-19 20:54:45 +0900

    yaku-kumi -> fonts

--- make_yakukumi.sh
+++ make_yakukumi.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-mkdir -p yaku-kumi
+mkdir -p fonts
 
 for file in `find noto-cjk/ -name '*.ttc'`; do
     outname=`basename $file`
     outname=${outname/Noto/YakuKumi}
     outname=${outname/CJK/}
     outname=${outname/ttc/otf}
-    pyftsubset ${file} --font-number=0 --text="、，。．〈〉《》「」『』【】〔〕（）［］｛｝" --layout-features='*' --output-file=yaku-kumi/${outname}
+    pyftsubset ${file} --font-number=0 --text="、，。．〈〉《》「」『』【】〔〕（）［］｛｝" --layout-features='*' --output-file=fonts/${outname}
     familyname=${outname}
     familyname=${familyname/YakuKumiSans-/Yaku Kumi Sans }
     familyname=${familyname/.otf/}
-    python3 fontname.py "${familyname}" yaku-kumi/${outname}
+    python3 fontname.py "${familyname}" fonts/${outname}
 done
