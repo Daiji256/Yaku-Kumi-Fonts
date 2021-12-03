05ef6f153e507ca207e4387324b0657956712e38
Daiji256 <butty.main@gmail.com>
2021-12-03 10:20:08 +0900

    、，。．・：；（）｟｠「」『』［］〚〛｛｝【】〖〗〈〉《》〔〕〘〙

--- make_yakukumi.sh
+++ make_yakukumi.sh
@@ -7,10 +7,10 @@ for file in `find noto-cjk/ -name '*.ttc'`; do
     outname=${outname/Noto/YakuKumi}
     outname=${outname/CJK/}
     outname=${outname/.ttc/}
-    pyftsubset ${file} --font-number=0 --text="、，。．〈〉《》「」『』【】〔〕（）［］｛｝" --layout-features='*' --output-file=fonts/${outname}.otf
+    pyftsubset ${file} --font-number=0 --text-file=yakumono.txt --layout-features='*' --output-file=fonts/${outname}.otf
     familyname=${outname}
     familyname=${familyname/YakuKumiSans-/Yaku Kumi Sans }
     python3 fontname.py "${familyname}" fonts/${outname}.otf
-    pyftsubset fonts/${outname}.otf --text="、，。．〈〉《》「」『』【】〔〕（）［］｛｝" --layout-features='*' --flavor=woff --output-file=fonts/${outname}.woff
-    pyftsubset fonts/${outname}.otf --text="、，。．〈〉《》「」『』【】〔〕（）［］｛｝" --layout-features='*' --flavor=woff2 --output-file=fonts/${outname}.woff2
+    pyftsubset fonts/${outname}.otf --text-file=yakumono.txt --layout-features='*' --flavor=woff --output-file=fonts/${outname}.woff
+    pyftsubset fonts/${outname}.otf --text-file=yakumono.txt --layout-features='*' --flavor=woff2 --output-file=fonts/${outname}.woff2
 done
